c04512a3656b9467d97d8b65c4ee44541e1eb19a
Alex Uceda <alexucedacintas@gmail.com>
2023-03-08 09:16:54 +0100

    first basic game done

---
+++ README.md
@@ -0,0 +1,20 @@
+TODO LIST:
+
+    -UNIFY HTMLS
+
+    -home screen styles
+    -game over screen styles
+
+    -select crosshair
+
+    -survival 
+        -select lives
+
+    -waves
+        -solapse objectives
+
+
+    BUGS:
+        -Content out of borders
+        -Score in game over screen
+        -delay last objective before game over screen
--- js/game.js
+++ js/game.js
@@ -1,25 +1,68 @@
+class Game{
+    constructor(){
+        this.counter=0
+        this.score=0
+    }
+    start(){
+        this.targets = document.getElementById("targets").value 
+        menu.remove()
+        const spawnObjective = setInterval(()=>{
+            if(this.counter < this.targets) {
+                const newObjective = new Objectives();
+                this.counter ++
+            }
+            else{
+                clearInterval(spawnObjective);
+                displayScore(this.score,this.targets);
+            }
+        },2000)
+    }
+    addScore(){
+        this.score ++;
+        console.log(this.score)
+    }
+
+}
 
 class Objectives {
     constructor(){
-        this.positionX = Math.round(Math.random()*200)
-        this.positionY = Math.round(Math.random()*100)
+        this.positionX = Math.round(Math.random()*200)-5
+        this.positionY = Math.round(Math.random()*100)-5
         this.createObjective();
+        this.eventListeners();
+        this.removingObjective();
     }
     createObjective(){
-        this.objectiveSpawn = document.createElement('div');
-        this.objectiveSpawn.setAttribute("class","objectives");
-        this.objectiveSpawn.setAttribute('style', `left:${this.positionX}vh; bottom:${this.positionY}vh`)
+        this.objective = document.createElement('div');
+        this.objective.setAttribute("class","objectives");
+        this.objective.setAttribute('style', `left:${this.positionX}vh; bottom:${this.positionY}vh`)
         this.boardElm = document.getElementById("board")
-        this.boardElm.appendChild(this.objectiveSpawn);
+        this.boardElm.appendChild(this.objective);
+    }    
+
+    removingObjective(){
+        const remove = setInterval(() => {
+            this.objective.remove();
+        }, 2000);
     }
+        
+    eventListeners(){
+        this.objective.addEventListener("click",e=>{
+            this.objective.setAttribute("class", "objectiveHit")
+            game.addScore();
+        })
+    }
+    
 }
-const allObjectives = []
-const spawnObjective = setInterval(()=>{
-    const newObjective = new Objectives();
-    allObjectives.push(newObjective)
-},2000)
+function displayScore(score,targets){
+    score_block = document.createElement("div")
+    score_block.className="menu"
+    score_block.innerHTML=`YOUR SCORE IS <br> ${score}/${targets}`
+    document.body.appendChild(score_block)
+}
 
 
-objectiveSpawn.addEventListener("click",e=>{
-    console.log('hola');
-})
+const start_btn = document.getElementById("play");
+const menu = document.getElementsByClassName("menu")[0];
+start_btn.addEventListener('click', () => game.start());
+const game = new Game();
