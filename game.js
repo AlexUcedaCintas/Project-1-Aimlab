e19fb0e6289bb426b7692e01d02c999a333bdf08
Alex Uceda <alexucedacintas@gmail.com>
2023-03-09 16:11:39 +0100

    responsibe implement

--- game.js
+++ game.js
@@ -1,77 +1,85 @@
-class Game{
-    constructor(){
-        this.counter=0
-        this.score=0
-        this.targets = document.getElementById("targets").value 
-        
+class Game {
+    constructor() {
+      this.counter = 0;
+      this.score = 0;
     }
-    start(){
-        this.speed = Number(document.querySelector('input[name="lvl"]:checked').value);
-        console.log(this.speed)
-        menu.remove()
-        const spawnObjective = setInterval(()=>{
-            if(this.counter < this.targets) {
-                console.log(this.speed)
-                const newObjective = new Objectives(this.speed);
-                this.counter ++
-            }
-            else{
-                clearInterval(spawnObjective);
-                displayScore(this.score,this.targets);
-            }
-        },this.speed)
+    start() {
+      this.boardElm = document.createElement("game");
+      this.boardElm.setAttribute("id","game")
+      this.body = document.getElementById("board")
+      this.body.appendChild(this.boardElm)
+      this.targets = document.getElementById("targets").value;
+      this.speed = Number(
+        document.querySelector('input[name="lvl"]:checked').value
+      );
+      menu.remove();
+      const spawnObjective = setInterval(() => {
+        if (this.counter < this.targets) {
+          const newObjective = new Objectives(this.speed);
+          this.counter++;
+        } else {
+          clearInterval(spawnObjective);
+          
+          this.displayScore(this.score, this.targets);
+        }
+      }, this.speed);
     }
-}
-
-class Objectives {
-    constructor(speed){
-        this.positionX = Math.floor(Math.random() * 200 );
-        this.positionY = Math.floor(Math.random() * 100 );
-        this.createObjective();
-        this.eventListeners();
-        this.removingObjective();
-        this.nohit=true;
-        this.speed = speed
+    displayScore(score, targets) {
+      this.boardElm.remove()
+      this.score_block = document.createElement("div");
+      this.score_block.className = "menu";
+      this.score_block.innerHTML = `<h1>YOUR SCORE IS</h1> <br> <h1 class="red">${score}/${targets}</h1><br><button class="button" onclick="location.reload()">PLAY AGAIN</button>`
+      this.body.appendChild(this.score_block);
     }
-    createObjective(){
-        this.objective = document.createElement('div');
-        this.objective.setAttribute("class","objectives");
-        this.objective.setAttribute('style', `left:${this.positionX}vh; bottom:${this.positionY}vh`)
-        this.boardElm = document.getElementById("board")
-        this.boardElm.appendChild(this.objective);
-    }    
-
-    removingObjective(){
-        const remove = setInterval(() => {
-            this.objective.remove();
-            this.nohit=true;
-        },game.speed);
+  }
+  
+  class Objectives {
+    constructor(speed) {
+      this.boardElm = document.getElementById("game");
+      this.boardRect = this.boardElm.getBoundingClientRect()
+      this.vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+      this.vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+      this.positionX = Math.floor(Math.random() * this.boardRect.width-50) + this.boardRect.left;
+      this.positionY = Math.floor(Math.random() * this.boardRect.height-50) + this.boardRect.top;
+  
+      this.createObjective();
+      this.eventListeners();
+      this.removingObjective();
+      this.nohit = true;
+      this.speed = speed;
     }
-        
-    eventListeners(){
-        this.objective.addEventListener("click",()=>{
-            if(this.nohit){
-                this.nohit=false;
-                this.objective.classList.add("objectiveHit")
-                game.score ++;
-            }
-        })
+    createObjective() {
+      this.objective = document.createElement("div");
+      this.objective.classList.add("objectives");
+      this.objective.style.left = this.positionX + "px";
+      this.objective.style.bottom = this.positionY + "px";
+      this.boardElm.appendChild(this.objective);
     }
-    
-}
-function displayScore(score,targets){
-    score_block = document.createElement("div")
-    score_block.className="menu"
-    score_block.innerHTML=`YOUR SCORE IS <br> ${score}/${targets}<br><button onclick="location.reload()">PLAY AGAIN</button>`
-    document.body.appendChild(score_block)
-}
-
-
-const start_btn = document.getElementById("play");
-const menu = document.getElementsByClassName("menu")[0];
-start_btn.addEventListener('click', () => game.start());
-const game = new Game();
-tarLabel = document.getElementById("targetLabel");
-targets.addEventListener("input",function(){
-    tarLabel.innerHTML=targets.value;
-})
+  
+    removingObjective() {
+      const remove = setInterval(() => {
+        this.objective.remove();
+        this.nohit = true;
+      }, game.speed);
+    }
+  
+    eventListeners() {
+      this.objective.addEventListener("click", () => {
+        if (this.nohit) {
+          this.nohit = false;
+          this.objective.classList.add("objectiveHit");
+          game.score++;
+        }
+      });
+    }
+  }
+  
+  
+  const start_btn = document.getElementById("play");
+  const menu = document.getElementsByClassName("menu")[0];
+  start_btn.addEventListener("click", () => game.start());
+  const game = new Game();
+  tarLabel = document.getElementById("targetLabel");
+  targets.addEventListener("input", function () {
+    tarLabel.innerHTML = targets.value;
+  });
